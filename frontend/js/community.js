05bf71f6bd509755cb4b2fd43b8d234ddb93305b
quocckhanhhxyz <qikayybattu@gmail.com>
2026-06-29 22:55:13 +0700

    fix(translation): translate subject tag label from vietnamese Môn to english Subject

--- frontend/js/community.js
+++ frontend/js/community.js
@@ -130,7 +130,7 @@ document.addEventListener("DOMContentLoaded", async function () {
       const subjectText = doc.subjectCode
         ? `${doc.subjectCode} - ${doc.subjectName}`
         : doc.subjectName;
-      subjectItem.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="12" width="12" stroke="currentColor" stroke-width="2.5" style="vertical-align: -1px; margin-right: 4px;"><path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82zM7 7h.01"/></svg> Môn: ${subjectText}`;
+      subjectItem.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="12" width="12" stroke="currentColor" stroke-width="2.5" style="vertical-align: -1px; margin-right: 4px;"><path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82zM7 7h.01"/></svg> Subject: ${subjectText}`;
       meta.append(subjectItem);
     }
 
