e36359635d358f7b082dbc0f35a4a83de10243df
quoctk06 <trankienquoc62@gmail.com>
2026-07-24 21:30:49 +0700

    chore: add missing mock to DocumentServiceTest

--- backend/src/test/java/com/demo/ai_study_hub/DocumentServiceTest.java
+++ backend/src/test/java/com/demo/ai_study_hub/DocumentServiceTest.java
@@ -74,6 +74,8 @@ class DocumentServiceTest {
     private com.demo.ai_study_hub.repository.DocumentReportRepository documentReportRepository;
     @Mock
     private com.demo.ai_study_hub.service.NotificationService notificationService;
+    @Mock
+    private com.demo.ai_study_hub.repository.SubjectRequestRepository subjectRequestRepository;
 
     @InjectMocks
     private DocumentService documentService;
